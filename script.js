a600f43782bea830578511f53cb6700975be087b
Hardik Kajne <hardikkajne@gmail.com>
2026-02-26 19:31:15 +0530

    Added squeezeMouse feature

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-let cursor = document.querySelector('#cursor');
+const cursor = document.querySelector('#cursor');
+let timer = 0;
+    let xprev = 0;
+    let yprev = 0;
+    let xscale = 1;
+    let yscale = 1;
 
-document.addEventListener('mousemove',(event)=>{
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.top = event.clientY + 'px';
-})
+function squeezeMouse(){
+    
+    window.addEventListener('mousemove',(event)=>{
+        clearTimeout(timer);
+
+        xscale = gsap.utils.clamp(0.6,1.4,event.clientX - xprev);
+        yscale = gsap.utils.clamp(0.6,1.4,event.clientY - yprev);
+        
+        xprev = event.clientX;
+        yprev = event.clientY;
+
+        cursor.style.transform = `translate(${event.clientX}px,${event.clientY}px) translate(-50%,-50%) scale(${xscale},${yscale})`;
+        
+        timer = setTimeout(()=>{
+            cursor.style.transform = `translate(${event.clientX}px,${event.clientY}px) translate(-50%,-50%) scale(1,1)`;
+        },100);
+    })
+}
+squeezeMouse();
